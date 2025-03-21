d7f4a0ea2e56f9c6eac1132457ea36e798cdddfe
ragasreya-choppa <ragasreyachoppa@gmail.com>
2025-03-20 22:36:11 -0600

    init commit

---
+++ src/com/sample/Printing.java
@@ -0,0 +1,10 @@
+package com.sample;
+
+public class Printing
+{
+    public static void main(String[]args)
+    {
+        // printing something in Java
+        System.out.println("This is IntelliJ IDEA");
+    }
+}
